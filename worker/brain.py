b98e5554c92d286309df156851f35827f053fc9b
john <john@example.com>
2016-06-20 15:00:42 +0200

    reorganize db models and introduce models table
    pipelining prepared, ready for retrain button
    
    pipeline done, factory ready to be integrated

--- worker/brain.py
+++ worker/brain.py
@@ -1,34 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from functools import lru_cache
-from brain.tagger import Tagger, to_sample
+from brain.tagger import TaggerFactory
 from brain.feature.language_detect import is_english
-from brain.feature.lemma_tokenizer import LemmaTokenTransformer
 from brain.feature.fingerprint import get_fingerprint
 
 from worker.celery import app
 
-tokenizer = LemmaTokenTransformer(short_url=True)
-
 
 @lru_cache(maxsize=64)
 def get_tagger(model_id):
-    return Tagger(model_id)
+    return TaggerFactory().name(model_id).tagger
 
 
 @app.task
-def predict(meta, model_id='default'):
-    if not all([key in meta for key in ('tokens', 'fingerprint')]):
-        return None
+def predict(text, fingerprint, model_id='default'):
     tagger = get_tagger(model_id)
-    return tagger.predict(to_sample(meta['tokens'], meta['fingerprint']))
+    return tagger.predict((text, fingerprint))
 
 
 @app.task
 def predict_text(text, model_id='default'):
     if not is_english(text):
         raise ValueError('text is not in english')
-    tokens = tokenizer(text)
     fingerprint = get_fingerprint(text).positions
     tagger = get_tagger(model_id)
-    return tagger.predict(to_sample(tokens, fingerprint))
+    return tagger.predict((text, fingerprint))
+
+
+@app.task
+def train_model(user_id, tagset_id, sources=None, name=None, params=None):
+    factory = (TaggerFactory()
+               .user_id(user_id)
+               .tagset(tagset_id)
+               .sources(sources)
+               .name(name)
+               .params(params)
+               .train()
+               .persist())
+    return factory.tagger.name
--- worker/meta_pipeline.py
+++ worker/meta_pipeline.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from brain.feature.language_detect import language_detect
-from brain.feature.lemma_tokenizer import LemmaTokenTransformer
 from brain.feature.fingerprint import get_fingerprints
 from db import DB, DBMapper, flag_modified, modifying
 from db.models.facebook import FacebookCommentEntry
@@ -22,23 +21,6 @@ def add_language(*_):
     mapper = DBMapper(FacebookCommentEntry)
     mapper.filter = (FacebookCommentEntry.meta['lang'].astext == None)
     mapper.each(_add_language)
-
-
-tokenizer = LemmaTokenTransformer(short_url=True)
-
-
-@modifying(keys=['meta'])
-def _add_tokens(entry: FacebookCommentEntry) -> FacebookCommentEntry:
-    tokens = tokenizer(entry.data['message'].replace('\b', ''))
-    entry.meta['tokens'] = tokens
-    return entry
-
-
-@app.task(trail=True)
-def add_tokens(*_):
-    mapper = DBMapper(FacebookCommentEntry)
-    mapper.filter = ((FacebookCommentEntry.meta['lang'].astext == 'en') & (FacebookCommentEntry.meta['tokens'] == None))
-    mapper.each(_add_tokens)
 
 
 class FingerprintHandler(object):
@@ -67,4 +49,4 @@ def add_fingerprint(*_):
 
 @app.task(trail=True)
 def meta_pipeline():
-    return (add_language.s() | add_tokens.s() | add_fingerprint.s())()
+    return (add_language.s() | add_fingerprint.s())()
